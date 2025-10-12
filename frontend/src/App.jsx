8540fb5119980d313f51f8ebea2c2f252cc0c28c
LOGA-KRISHNA <logakrishnad@gmail.com>
2025-10-12 16:25:43 +0000

    Fresh start for food delivory app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,10 @@
 import { useState } from 'react'
 import './App.css'
-import { HeroSection } from './components/HeroSection/HeroSection';
-import NewArrivals from './components/Section/NewArrivals';
 
 function App() {
   return (
     <>
-      <HeroSection />
-      <NewArrivals/>
+      <h1>Hello</h1>
     </>
   )
 }
